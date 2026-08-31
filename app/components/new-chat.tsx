257b9535a16c761523f47ce7a452b9937bd3d9c6
lzk <123@qq.com>
2026-08-31 17:51:41 +0800

    udpate

--- app/components/new-chat.tsx
+++ app/components/new-chat.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useRef, useState } from "react";
-import { Path, SlotID } from "../constant";
+import { Path } from "../constant";
 import { IconButton } from "./button";
 import { EmojiAvatar } from "./emoji";
 import styles from "./new-chat.module.scss";
@@ -32,59 +31,14 @@ function MaskItem(props: { mask: Mask; onClick?: () => void }) {
   );
 }
 
-function useMaskGroup(masks: Mask[]) {
-  const [groups, setGroups] = useState<Mask[][]>([]);
-
-  useEffect(() => {
-    const computeGroup = () => {
-      const appBody = document.getElementById(SlotID.AppBody);
-      if (!appBody || masks.length === 0) return;
-
-      const rect = appBody.getBoundingClientRect();
-      const maxWidth = rect.width;
-      const maxHeight = rect.height * 0.6;
-      const maskItemWidth = 120;
-      const maskItemHeight = 50;
-
-      const randomMask = () => masks[Math.floor(Math.random() * masks.length)];
-      let maskIndex = 0;
-      const nextMask = () => masks[maskIndex++ % masks.length];
-
-      const rows = Math.ceil(maxHeight / maskItemHeight);
-      const cols = Math.ceil(maxWidth / maskItemWidth);
-
-      const newGroups = new Array(rows)
-        .fill(0)
-        .map((_, _i) =>
-          new Array(cols)
-            .fill(0)
-            .map((_, j) => (j < 1 || j > cols - 2 ? randomMask() : nextMask())),
-        );
-
-      setGroups(newGroups);
-    };
-
-    computeGroup();
-
-    window.addEventListener("resize", computeGroup);
-    return () => window.removeEventListener("resize", computeGroup);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return groups;
-}
-
 export function NewChat() {
   const chatStore = useChatStore();
   const maskStore = useMaskStore();
 
   const masks = maskStore.getAll();
-  const groups = useMaskGroup(masks);
 
   const navigate = useNavigate();
   const config = useAppConfig();
-
-  const maskRef = useRef<HTMLDivElement>(null);
 
   const { state } = useLocation();
 
@@ -105,13 +59,6 @@ export function NewChat() {
       }
     },
   });
-
-  useEffect(() => {
-    if (maskRef.current) {
-      maskRef.current.scrollLeft =
-        (maskRef.current.scrollWidth - maskRef.current.clientWidth) / 2;
-    }
-  }, [groups]);
 
   return (
     <div className={styles["new-chat"]}>
@@ -169,17 +116,9 @@ export function NewChat() {
         />
       </div>
 
-      <div className={styles["masks"]} ref={maskRef}>
-        {groups.map((masks, i) => (
-          <div key={i} className={styles["mask-row"]}>
-            {masks.map((mask, index) => (
-              <MaskItem
-                key={index}
-                mask={mask}
-                onClick={() => startChat(mask)}
-              />
-            ))}
-          </div>
+      <div className={styles["masks"]}>
+        {masks.map((mask) => (
+          <MaskItem key={mask.id} mask={mask} onClick={() => startChat(mask)} />
         ))}
       </div>
     </div>
